63b3b8491cb61ab383e98a9e6b15fbb34068fc1f
Nikolay Korobeynikov <Korobeynikov.n@gmail.com>
2020-12-04 22:58:34 +0300

    HW_1

---
+++ test1/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by Николай Коробейников on 30.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+    @IBAction func myActionnn(_ sender: Any) {
+        print("пользователь нажал синюю кнопку")
+    }
+}
+
