e04c65110f77e64df80dc7cbc88fddd034661205
Nikolay Korobeynikov <Korobeynikov.n@gmail.com>
2020-12-10 20:46:35 +0300

    hw2
    hw2

--- test1/ViewController.swift
+++ test1/ViewController.swift
@@ -8,16 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let login = "admin"
+    private let pass = "123"
+    
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     }
 
-
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue){
+        print("exit button")
+    }
+        
     @IBAction func myActionnn(_ sender: Any) {
         print("пользователь нажал синюю кнопку")
+    }
+    
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+          
+      if identifier == "Login" {
+          if checkInput() {
+              return true
+          }
+          else {
+            let alert = UIAlertController(
+                title: "Ошибка входа",
+                message: "Неправильно введен логин или пароль",
+                preferredStyle:  .alert)
+            let action = UIAlertAction(title: "Хорошо",
+                                    style: .default)
+                {(action) in
+                        self.loginField.text=""
+                        self.passField.text=""
+                }
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+            return false
+          }
+      }
+          
+    print("another segue")
+    return true
+    }
+      
+    private func checkInput() -> Bool {
+        return (loginField.text==login)&&(passField.text==pass)
     }
 }
 
